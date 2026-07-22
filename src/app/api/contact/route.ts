053783f2001060af10c521b149510034762bb25b
unknown <zurrilabs@gmail.com>
2026-07-22 02:51:06 -0300

    fix prisma client import

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,4 +1,13 @@
-import { prisma } from "@/lib/prisma";
+import { PrismaClient } from "@/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
+
+const prisma = new PrismaClient({
+  adapter,
+});
 
 export async function POST(request: Request) {
   try {
